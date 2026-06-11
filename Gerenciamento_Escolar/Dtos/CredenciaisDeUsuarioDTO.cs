880ecb905cb47c7713b4bcf0f2fe77de03795ff6
gabriel <gabriel14fav@gmail.com>
2026-06-11 16:01:43 -0300

    Adicionar imports

--- Gerenciamento_Escolar/Dtos/CredenciaisDeUsuarioDTO.cs
+++ Gerenciamento_Escolar/Dtos/CredenciaisDeUsuarioDTO.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Gerenciamento_Escolar.Dtos;
 
 public record CredenciaisDeUsuarioDTO([Required(AllowEmptyStrings = false)]string nome,[Required(AllowEmptyStrings = false)] string senha);
